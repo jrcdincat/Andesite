52ce9d85f18447f54a1043c2bd374c49c9635d7d
DESKTOP-ADFBECM\jrcdi <jrcdincat@gmail.com>
2021-06-19 22:56:47 -0400

    Update object class and add interface

--- Andesite/src/GameObject.cpp
+++ Andesite/src/GameObject.cpp
@@ -1,11 +1,3 @@
 #include "pch.h"
 #include "GameObject.h"
 
-
-GameObject::GameObject() {
-
-}
-
-GameObject::~GameObject() {
-
-}
--- Andesite/src/GameObject.h
+++ Andesite/src/GameObject.h
@@ -1,8 +1,23 @@
 #pragma once
-class GameObject {
+#include "IObject.h"
+#include "Math.h"
+
+class GameObject : public IObject {
 public: 
-	GameObject(); 
-	~GameObject(); 
-protected: 
+	GameObject(std::string texID, int x, int y, int w, int h, SDL_RendererFlip flip = SDL_FLIP_NONE)
+		:textureID(texID), width(w), height(h), renderFlip(flip) { 
+		position.x = x; 
+		position.y = y;
+	};
+	virtual ~GameObject() {};
+
+	virtual void Draw() = 0;
+	virtual void Update(float dt) = 0;
+	virtual void Clean() = 0;
 	
+protected:
+	Vector2f position; 
+	int width, height; 
+	std::string textureID; 
+	SDL_RendererFlip renderFlip; 
 };
